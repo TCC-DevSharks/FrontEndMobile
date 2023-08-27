c8fa3ec669629e9c0a811bdfe044c1c87a6a2d5a
Cleiton <cleitonncruz105@gmail.com>
2023-08-27 07:49:14 -0300

    Validacao date

--- app/src/main/java/br/senai/sp/jandira/tcc/gui/Register/RegisterScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/tcc/gui/Register/RegisterScreen.kt
@@ -1,21 +1,30 @@
 package br.senai.sp.jandira.tcc.gui.Register
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -38,6 +47,8 @@ fun RegisterScreen(navController: NavController, viewModel: ModelRegister) {
     var nome by rememberSaveable { mutableStateOf("") }
     var dataNascimento by rememberSaveable { mutableStateOf("") }
     var telefone by rememberSaveable { mutableStateOf("") }
+    var visible by remember { mutableStateOf(false) }
+
 
     Column (modifier = Modifier
         .fillMaxSize()
@@ -63,11 +74,32 @@ fun RegisterScreen(navController: NavController, viewModel: ModelRegister) {
 
         Profile(size = 24.dp)
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
         Column (modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally) {
 
+            AnimatedVisibility(
+                visible = visible,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                enter = fadeIn(
+                    initialAlpha = 0.4f
+                ),
+                exit = fadeOut(
+                    animationSpec = tween(durationMillis = 250)
+                )
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = stringResource(id = R.string.empty_fields),
+                        color = Color.Red
+
+                    )
+                }
+            }
 
             OutlinedTextFieldTodos(
                 texto = R.string.text_field_nome,
@@ -119,10 +151,9 @@ fun RegisterScreen(navController: NavController, viewModel: ModelRegister) {
                     viewModel.email = email
                     viewModel.telefone = telefone
                     viewModel.data_nascimento = date
-                    Log.i("rty", "${date}")
                     navController.navigate("register_password")
                 }else{
-                    Log.i("erro", "todos os campos devem ser preenchidas")
+                    visible = true
                 }
 
 
