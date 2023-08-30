22a5388ae9ed0d5c21f46137f040d16938c9d39e
GUILHERME DE OLIVEIRA LIMA <22282086@dsn127m1249388.cfp127aluno.local>
2023-08-30 11:24:33 -0300

    Entrega

--- app/src/main/java/br/senai/sp/jandira/tcc/gui/MaternityBag/MaternityBagScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/tcc/gui/MaternityBag/MaternityBagScreen.kt
@@ -1,6 +1,7 @@
 package br.senai.sp.jandira.tcc.gui.MaternityBag
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,14 +11,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,15 +39,19 @@ import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.tcc.R
 import br.senai.sp.jandira.tcc.componentes.Header
 import androidx.navigation.NavController
+import br.senai.sp.jandira.tcc.componentes.CardAlong
 import br.senai.sp.jandira.tcc.componentes.SubHeader
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MaternityBagScreen() {
 
     Column(modifier = Modifier.fillMaxSize()) {
 
-        val expanded = remember { mutableStateOf(false) }
+
+        var isChecked by remember { mutableStateOf(false) }
+
 
 
         Header(
@@ -48,133 +61,83 @@ fun MaternityBagScreen() {
         )
 
         SubHeader(
-            leftText = stringResource(id = R.string.my_list),
-            rightText = stringResource(id = R.string.all)
+            leftText = stringResource(id = R.string.suggested),
+            rightText = stringResource(id = R.string.my_list)
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .verticalScroll(rememberScrollState())
+//        ) {
+//
+//            CardAlong()
+//            CardAlong()
+//        }
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
 
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 22.dp, vertical = 7.dp),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .padding(19.dp)
-                        .background(Color.Yellow)
-                        .clickable {
+                Checkbox(
+                    modifier = Modifier.height(30.dp),
+                    checked = isChecked,
+                    onCheckedChange = { isChecked = it },
+                    colors = CheckboxDefaults.colors(
+                        checkmarkColor = Color(182, 182, 246),
+                        checkedColor = Color(182, 182, 246), // Cor quando marcado
+                        uncheckedColor = Color(182, 182, 246) // Cor quando não marcado
+                    )
+                )
+                Spacer(modifier = Modifier.width(15.dp))
 
-                            expanded.value = !expanded.value
-                        }
-
-                ) {
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-
-                        Image(painter = painterResource(id = R.drawable.baseline_add_circle_65),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .clickable {
-//                                navController.navigate(rota)
-                                }
-                                .size(35.dp)
-                        )
-
-                        Spacer(modifier = Modifier.width(10.dp))
-
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            text = "Mamadeiras e Bicos",
-                            fontSize = 16.8.sp,
-                            fontWeight = FontWeight(700),
-                        )
-                    }
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(1f)
-                            .padding(
-                                vertical = 5.dp,
-                                horizontal = 14.dp
-                            )
-                    ) {
-
-                        Text(if (expanded.value) "Show lfghdfghdfgdfgdfgdfgdfgdfgdfgdfgdfgdfggggggggggess" else "ffff")
-
-                    }
-
-
-                }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .padding(19.dp)
-                        .background(Color.Yellow)
-                        .clickable {
-
-                            expanded.value = !expanded.value
-                        }
-
-                ) {
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-
-                        Image(painter = painterResource(id = R.drawable.baseline_add_circle_65),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .clickable {
-//                                navController.navigate(rota)
-                                }
-                                .size(35.dp)
-                        )
-
-                        Spacer(modifier = Modifier.width(10.dp))
-
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            text = "Mamadeiras e Bicos",
-                            fontSize = 16.8.sp,
-                            fontWeight = FontWeight(700),
-                        )
-                    }
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(1f)
-                            .padding(
-                                vertical = 5.dp,
-                                horizontal = 14.dp
-                            )
-                    ) {
-
-                        Text(if (expanded.value) "Show lfghdfghdfgdfgdfgdfgdfgdfgdfgdfgdfgdfggggggggggess" else "ffff")
-
-                    }
-
-
-                }
-
+                Text(text = "Mamadeiras e Bicos",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(400),
+                )
 
             }
 
 
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 22.dp, vertical = 7.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Checkbox(
+                    modifier = Modifier.height(30.dp),
+                    checked = isChecked,
+                    onCheckedChange = { isChecked = it },
+                    colors = CheckboxDefaults.colors(
+                        checkmarkColor = Color(182, 182, 246),
+                        checkedColor = Color(182, 182, 246), // Cor quando marcado
+                        uncheckedColor = Color(182, 182, 246) // Cor quando não marcado
+                    )
+                )
+                Spacer(modifier = Modifier.width(15.dp))
+
+                Text(text = "Mamadeiras e Bicos",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(400),
+                )
+
+            }
+
         }
+
+
+
 
 
     }
